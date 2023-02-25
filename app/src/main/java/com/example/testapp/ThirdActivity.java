bfb85c800d2e77b8cbe0ea67e15bd677258a3d1c
Vince Regle <staphmaster2002@gmail.com>
2023-02-25 14:07:29 +0800

    2:07pm FINISH Buttons Activity 2/25/23

--- app/src/main/java/com/example/testapp/ThirdActivity.java
+++ app/src/main/java/com/example/testapp/ThirdActivity.java
@@ -1,11 +1,14 @@
 package com.example.testapp;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 public class ThirdActivity extends AppCompatActivity {
 
@@ -13,7 +16,18 @@ public class ThirdActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+
+        Button button = (Button) findViewById(R.id.buttonThird);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "Button is Clicked: ");
+                Intent intent = new Intent(ThirdActivity.this, helloAndroid.class);
+                startActivity(intent);
+            }
+        });
     }
+
 
 
 }
